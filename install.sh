456dc563af6228623a9f57f4358ba281924c5ff4
Morris Horesh <morris_horesh@yahoo.com>
2022-02-27 16:48:27 +0200

    Add commit

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ docker cp checkip.sh $1:/checkip.sh
 docker exec $1 apt update
 docker exec $1 apt install curl openvpn transmission-daemon -y
 docker exec -it $1 adduser --no-create-home --disabled-password -q transmission
-
+docker commit $1 transmission
