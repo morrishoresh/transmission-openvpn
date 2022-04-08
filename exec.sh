249aa16c86059394061b3b3ad37e537849ab4d74
Morris Horesh <morris_horesh@yahoo.com>
2022-04-08 18:31:06 +0300

    Corrections

--- exec.sh
+++ exec.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
+if test -n "$1"
+then
+	TRANSMISSION_USER=$1
+else
+	TRANSMISSION_USER=transmission
+fi
+
+TRANSMISSION_HOME_DIR=$(getent passwd "$TRANSMISSION_USER" | cut -d: -f6)
+
+if test -z "$TRANSMISSION_HOME_DIR"
+then
+	echo Invalid user: $TRANSMISSION_USER
+	exit 1
+fi
+
 docker run \
 -v /etc/openvpn:/etc/openvpn \
--v /home/transmission:/home/transmission \
--e XUID=$(id -u transmission)  \
--e XGID=$(id -g transmission) \
+-v /home/transmission:$TRANSMISSION_HOME_DIR \
+-e XUID=$(id -u $TRANSMISSION_USER)  \
+-e XGID=$(id -g $TRANSMISSION_USER) \
 -e DNS1=1.1.1.1 \
 -e DNS2=84.200.70.40 \
 -e DNS3=1.0.0.1 \
@@ -14,4 +29,4 @@ docker run \
 --rm --privileged -d \
 transmission /init.sh
 
-
+exit $?
