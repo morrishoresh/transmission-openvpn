c3351448f27a17b6addaf0dcb48abc951300fec6
Morris Horesh <morris_horesh@yahoo.com>
2022-09-15 21:27:16 +0300

    Update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+
 docker image rm transmission 2>/dev/null
-docker cp init.sh $1:/init.sh
-docker cp checkip.sh $1:/checkip.sh
-docker exec $1 apt update
-docker exec $1 apt install curl openvpn transmission-daemon -y
-docker exec -it $1 adduser --no-create-home --disabled-password -q transmission
-docker commit $1 transmission
+docker rm -f transmission-init 2>/dev/null
+docker run -dit --name=transmission-init --rm ubuntu bash
+docker cp init.sh transmission-init:/init.sh
+docker cp checkip.sh transmission-init:/checkip.sh
+docker exec transmission-init apt update
+docker exec transmission-init apt install curl openvpn transmission-daemon -y
+docker exec -it transmission-init adduser --no-create-home --disabled-password -q transmission
+docker commit transmission-init transmission
+docker rm -f transmission-init
