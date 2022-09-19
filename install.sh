5be4b000dd058a9198a9efb236a5ab0209f55b76
Morris Horesh <morris_horesh@yahoo.com>
2022-09-19 21:14:50 +0300

    Make base image configurable

--- install.sh
+++ install.sh
@@ -1,12 +1,34 @@
 #!/bin/sh
 
+if test -z "$1"
+then
+	DOCKER_BASE_IMAGE=ubuntu
+else
+	DOCKER_BASE_IMAGE=$1
+fi
+
+#remove exiting transmission image
 docker image rm transmission 2>/dev/null
+
+#make sure there is not init image
 docker rm -f transmission-init 2>/dev/null
-docker run -dit --name=transmission-init --rm ubuntu bash
+
+#run init image
+docker run -dit --name=transmission-init --rm $DOCKER_BASE_IMAGE bash
+
+#copy files
 docker cp init.sh transmission-init:/init.sh
 docker cp checkip.sh transmission-init:/checkip.sh
+
+#install packages
 docker exec transmission-init apt update
 docker exec transmission-init apt install curl openvpn transmission-daemon -y
+
+#add transmission user
 docker exec -it transmission-init adduser --no-create-home --disabled-password -q transmission
+
+#create transmission image
 docker commit transmission-init transmission
+
+#cleanup
 docker rm -f transmission-init
