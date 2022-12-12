6b3bd48e8b80b11f1dd58aadf144b56d296ea7ab
Morris Horesh <morris_horesh@yahoo.com>
2022-12-12 23:11:43 +0200

    Add comment

--- monitor.sh
+++ monitor.sh
@@ -6,6 +6,9 @@ do
 	IP=$(/checkip.sh)
 	if test -z "$IP"
 	then
+		# When this happens it is usually an indication that there is a problem with the VPN
+		# connection. In this case we stop the vpn which will cause the init.sh script to
+		# recreate a vpn session
 		TPID=$(ps -ef | grep openvpn | grep transmission | awk '{print $2'})
 		kill $TPID
 	fi
