0d489b4cafc79249bf62b384e2036ebda7f449f5
Morris Horesh <morris_horesh@yahoo.com>
2022-12-14 20:51:04 +0200

    Correct monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -9,7 +9,7 @@ do
 		# When this happens it is usually an indication that there is a problem with the VPN
 		# connection. In this case we stop the vpn which will cause the init.sh script to
 		# recreate a vpn session
-		TPID=$(ps -ef | grep openvpn | grep transmission | awk '{print $2'})
+		TPID=$(ps -ef | grep openvpn | grep transmission | awk '{print $2}')
 		kill $TPID
 	fi
 done
